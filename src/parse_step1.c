375cf84ff60498017f3305cde9c150d7973f5aaf
Charles BERGANZA <cberganz@e2r11p22.clusters.42paris.fr>
2022-02-20 18:27:09 +0100

    parse_step1 ok -> memory leak to solve

--- src/parse_step1.c
+++ src/parse_step1.c
@@ -6,84 +6,72 @@
 /*   By: cberganz <cberganz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 18:24:31 by cberganz          #+#    #+#             */
-/*   Updated: 2022/02/20 16:17:17 by cberganz         ###   ########.fr       */
+/*   Updated: 2022/02/20 18:24:30 by cberganz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	set_control_op(t_list **lst, char *operator)
+static void	set_control_op(t_list **lst, char *input)
 {
 	t_list	*tmp;
+	int		i;
 
-	tmp = *lst;
-	tmp = tmp->next;
+	tmp = (*lst)->next;
+	i = 0;
 	while (tmp)
 	{
-		if (((t_command *)tmp->content)->control_op == NULL)
-			((t_command *)tmp->content)->control_op = operator;
+		while (input[i])
+		{
+			if (input[i] == '|' && input[i + 1] == '|')
+			{
+				((t_command *)tmp->content)->control_op = "||";
+				i += 2;
+				break ;
+			}
+			if (input[i] == '&' && input[i + 1] == '&')
+			{
+				((t_command *)tmp->content)->control_op = "&&";
+				i += 2;
+				break ;
+			}
+			i++;
+		}
 		tmp = tmp->next;
 	}
 }
 
-static void	*parse_doubleand(t_list **lst)
+static void iter_trim(t_list **lst)
 {
-	t_command	*tmp;
-	t_list		*tmp_lst;
-	t_list		*previous;
-	char		**tmp_char;
-	int			i;
+	t_list	*tmp_lst;
+	char	*tmp_string;
 
 	tmp_lst = *lst;
 	while (tmp_lst)
 	{
-		i = 1;
-		if (ft_strstr(*((t_command *)tmp_lst->content)->command, "&&"))
-			tmp_char = ft_split(*((t_command *)tmp_lst->content)->command, "&&");
-		else
-		{
-			previous = tmp_lst;
-			tmp_lst = tmp_lst->next;
-			continue ;
-		}
-		while (*tmp_char)
-		{
-			if (mem_alloc(sizeof(t_command), (void **)&tmp))
-				return (NULL);
-			tmp->command = &(*tmp_char);
-			if (i == 1)
-				tmp->control_op = "||";
-			else	
-				tmp->control_op = NULL;
-			ft_lstadd_here(previous->next, ft_lstnew(tmp));	
-			previous = tmp_lst;
-			tmp_lst = tmp_lst->next;
-			tmp_char++;
-			i++;
-		}
-		previous = tmp_lst;
+		tmp_string = ft_strtrim(((t_command *)tmp_lst->content)->command, " ");
+		mem_remove(((t_command *)tmp_lst->content)->command);
+		((t_command *)tmp_lst->content)->command = tmp_string;
 		tmp_lst = tmp_lst->next;
 	}
-	set_control_op(lst, "&&");
-	return ((void *)1);
 }
 
-static void	*parse_doublepipe(char *input, t_list **lst)
+static void	*parse(char *input, t_list **lst)
 {
 	char		**splited_input;
 	t_command	*tmp;
 
-	splited_input = ft_split(input, "||");
+	splited_input = ft_split_double(input, "||", "&&");
 	while (*splited_input)
 	{
 		if (mem_alloc(sizeof(t_command), (void **)&tmp))
 			return (NULL);
-		tmp->command = splited_input;
+		tmp->command = *splited_input;
 		tmp->control_op = NULL;
 		ft_lstadd_back(lst, ft_lstnew(tmp));
 		splited_input++;
 	}
-	set_control_op(lst, "||");
+	set_control_op(lst, input);
 	return ((void *)1);
 }
 
@@ -92,10 +80,9 @@ t_list	*parse_step1(char *input)
 	t_list		*command_list;
 
 	command_list = NULL;
-	if (!parse_doublepipe(input, &command_list))
+	if (!parse(input, &command_list))
 		return (NULL);
-	if (!parse_doubleand(&command_list))
-		return (NULL);
+	iter_trim(&command_list);
 	return (command_list);
 }
 
@@ -104,7 +91,7 @@ int	main(void)
 	t_list	*list;
 	t_list	*first;
 
-	list = parse_step1("test1 || test2 && test3 & test4 || test5");
+	list = parse_step1("test1 || test2 && test3 & test4 || test5 && test6 || test7");
 	first = list;
 	while (list)
 	{
@@ -112,14 +99,52 @@ int	main(void)
 			printf("%s\n", (char *)((t_command *)list->content)->control_op);
 		else
 			printf("(null)\n");
-		printf("%s\n\n", (char *)*((t_command *)list->content)->command);
+		printf("%s\n\n", (char *)((t_command *)list->content)->command);
 		list = list->next;
 	}
-	ft_lstclear(&first, free);
+	//ft_lstclear(&first, free);
+	del_garbage();
 	return (0);
 }
 
-//char	**parse_pipes()
+//static void	*parse_doubleand(t_list **lst)
 //{
+//	t_command	*tmp;
+//	t_list		*tmp_lst;
+//	t_list		*previous;
+//	char		**tmp_char;
+//	int			i;
 //
+//	tmp_lst = *lst;
+//	while (tmp_lst)
+//	{
+//		i = 1;
+//		if (ft_strstr(*((t_command *)tmp_lst->content)->command, "&&"))
+//			tmp_char = ft_split(*((t_command *)tmp_lst->content)->command, "&&");
+//		else
+//		{
+//			previous = tmp_lst;
+//			tmp_lst = tmp_lst->next;
+//			continue ;
+//		}
+//		while (*tmp_char)
+//		{
+//			if (mem_alloc(sizeof(t_command), (void **)&tmp))
+//				return (NULL);
+//			tmp->command = &(*tmp_char);
+//			if (i == 1)
+//				tmp->control_op = "||";
+//			else	
+//				tmp->control_op = NULL;
+//			ft_lstadd_here(previous->next, ft_lstnew(tmp));	
+//			previous = tmp_lst;
+//			tmp_lst = tmp_lst->next;
+//			tmp_char++;
+//			i++;
+//		}
+//		previous = tmp_lst;
+//		tmp_lst = tmp_lst->next;
+//	}
+//	set_control_op(lst, "&&");
+//	return ((void *)1);
 //}
